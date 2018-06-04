20a0752f69c9665f4fdc5685956c631a9f150e2c
'zxd' <zxd@zxd.com>
2018-06-04 18:52:44 +0800

    oauthAll

---
+++ src/main/java/com/example/demo/OAuthAllApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OAuthAllApplication  extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		//SpringApplication.run(OAuthAllApplication.class, args);
+		SpringApplication.run(OAuthAllApplication.class, args);
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	}
+}
